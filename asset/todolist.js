7a080ee813ae406885af88a2058180819855b32b
cpfl300 <cpfl300@naver.com>
2014-07-24 14:27:07 +0900

    2주차 CSS transition까지

--- asset/todolist.js
+++ asset/todolist.js
@@ -1,52 +1,81 @@
 var ENTER_KEYCODE = 13;
 
-function makeTODO(todo){
-	//template library로 개선하기
-	var li = document.createElement("li");
-	var div = document.createElement("div");
-	div.className = "view";
-	var input = document.createElement("input");
-	input.className = "toggle";
-	input.setAttribute("type", "checkbox");
-	var label = document.createElement("label");
-	label.innerText = todo;
-	var button = document.createElement("button");
-	button.className = "destroy";
-
-	div.appendChild(input);
-	div.appendChild(label);
-	div.appendChild(button);
-
-	li.appendChild(div);
-
-	return li;
-}
-
 function addTODO(e){
 	if(e.keyCode == ENTER_KEYCODE){
 		//만들어서 추가 함
-
-		var input ={
-			inputData: document.querySelector("#new-todo").value
+		var data = {
+			todo: document.querySelector("#new-todo").value
 		};
 
 		var template = 
-		"<li class=\"{}\">" +
+		"<li class=\"appending\">" +
 			"<div class=\"view\">" +
-				"<input class=\"toggle\" type=\"checkbox\" {}>" +
-				"<label>{{inputData}}</label>" +
+				"<input class=\"toggle\" type=\"checkbox\">" +
+				"<label>{{todo}}</label>" +
 				"<button class=\"destroy\"></button>"+
 			"</div>" +
 		"</li>";
 
-		var result = Mustache.to_html(template, input);
+		var result = Mustache.to_html(template, data);
 		document.querySelector("#todo-list").insertAdjacentHTML('afterBegin', result);
+		//prepend 구현
 
+		var appended = document.querySelector("#todo-list").firstElementChild;
+		appended.offsetHeight;
+		appended.className = "";
+		//추가 시 opacity조정
+		/*
+		appended.style.opacity = 0;
+		var i = 0;
+		var key = setInterval(function(){
+			if(i===50){
+				clearInterval(key);
+			}else{
+				appended.style.opacity = i * 0.02;	
+			}
+			i++;
+		}, 16);
+		*/
 		document.querySelector("#new-todo").value = "";	
+	}
+}
+
+// 체크박스 
+function completedTODO(e){
+	// input[type=checkbox]의 엘리먼트에서 checked라는 속성 확인!
+	var input = e.target;
+	var li = input.parentNode.parentNode;
+	if(input.checked){
+		//li ele에 completed 클래스를 추가
+		li.className = "completed";
+	}else{
+		li.className = "";
+	}
+	// e.target은 이벤트를 실제로 발생시킨 ele, e.currentTarget은 이벤트를 바인딩한 ele
+
+	e.target
+}
+
+// 삭제버튼
+function removeTODO(e){
+	var li = e.target.parentNode.parentNode;
+	li.className = "deleting";
+
+	li.addEventListener("transitionend", function(){
+		li.parentNode.removeChild(li);
+	})
+}
+
+function liClicked(e){
+	if(e.target.tagName === "INPUT"){
+		completedTODO(e);
+	} else if (e.target.tagName === "BUTTON"){
+		removeTODO(e)
 	}
 }
 
 // #keydown (enter) 이벤트에 li를 추가하는 함수를 등록
 document.addEventListener("DOMContentLoaded", function(){
 	document.querySelector("#new-todo").addEventListener("keydown", addTODO);
+	document.querySelector('ul').addEventListener("click", liClicked);
 });
